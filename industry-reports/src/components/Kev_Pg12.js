46bdedab2133efc30bc5032bf84373edfaa5607b
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-29 17:35:29 -0500

    added paragraph, and h1/h2 tags,

--- industry-reports/src/components/Kev_Pg12.js
+++ industry-reports/src/components/Kev_Pg12.js
@@ -10,7 +10,12 @@ class Kev_Pg12 extends React.Component {
     render() {
         return (
             <div>
-                <h2> Hello Class </h2>
+                <h1 className="kev-title-politic"> POLITICAL PARTICIPATION </h1>
+                    <h2 id="side-pg-div"> Platforms are more likely to remove bots because they violate their terms of
+                    service rather than to protect users’ right to political participation. 
+                    </h2>
+                    <p id="kev-paragraph">  Bots have many useful purposes, including helping search engines find content. Because AI is being designed to mimic human behavior in online conversations, detecting those online bots that are weaponized to spread disinformation in political discourse could become more difficult.</p>
+
             </div>
         );
     }
