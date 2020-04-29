840264e11e63eff5265901831ca78e0000bdc244
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-04-29 18:37:38 -0400

    established shell

--- industry-reports/src/App.js
+++ industry-reports/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Equality from './components/Equality';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() { 
+      const equality = () => {
+      console.log('connected')
+    }
+    return (  
+      <React.Fragment>
+      <h1> A HUMAN RIGHTS FRAME FOR AI RISKS AND HARM </h1>
+      <div className="topic-box">
+        <div id='equality' > <h1 onClick={() => equality()} > equality</h1> </div>
+        <div id='political' > political </div>
+        <div id='privacy' > privacy </div>
+        <div id='freedom' > freedom </div>
+        <div id='disability' > disability </div>
+      </div>
+      <h1> About </h1>
+      </React.Fragment>
+    );
+  }
 }
-
+ 
 export default App;
