779d833858faccbc22f601f3225a96d851174bdc
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-04-29 18:39:10 -0400

    added equality component

---
+++ industry-reports/src/components/Equality.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import image from '../assets/placeholder-image.jpg';
+
+const Equality = () => {
+    return ( 
+    <div className='main-box' >
+      <div>
+      <img id='image' src={image} alt='science'/>
+      </div>
+      <div>
+      <h1 id ='something'> Nondiscrimination and Equality </h1>
+      <p> 
+        As mentioned above, bias and discrimination have become central topics for those concerned with the governance and social impact of AI systems.20 A number of high profile studies have demonstrated that, as in the case of detecting skin color, certain AI systems are inherently discriminatory. Alarming reports have detailed how discrim-inatory algorithms are already deployed in the justice system, wherein judges use these tools for sentencing that purport to predict the likelihood a criminal defendant will reoffend 
+      </p>
+      </div>
+    </div>
+     );
+}
+ 
+export default Equality;
