a559fe7a72d66e4e8d4599e4af308e4d8bb8cfcf
dev-augustin <dev.augustin00@gmail.com>
2020-04-30 11:36:32 -0500

    disability.js with onclick:

---
+++ industry-reports/src/components/Disability.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Disability(props) {
+    //const textClick = this.props.textClick
+    return (
+        <div>
+            <h1 onClick={props.clickText}>{props.title}</h1>
+            {/* <p> lorem20 cxzc xz xz</p> */}
+        </div>
+    )
+}
+
+{/* <div className='main-box' >
+<div>
+<img id='image' src={image} alt='science'/>
+</div>
+<div>
+<h1 id ='something'> Nondiscrimination and Equality </h1>
+<p> 
+  As mentioned above, bias and discrimination have become central topics for those concerned with the governance and social impact of AI systems.20 A number of high profile studies have demonstrated that, as in the case of detecting skin color, certain AI systems are inherently discriminatory. Alarming reports have detailed how discrim-inatory algorithms are already deployed in the justice system, wherein judges use these tools for sentencing that purport to predict the likelihood a criminal defendant will reoffend 
+</p>
+</div>
+</div> */}
