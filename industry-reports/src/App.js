4d1fd00e0cdd43d31c52933be5cd8bf37a80ae1c
dev-augustin <dev.augustin00@gmail.com>
2020-04-30 19:38:42 -0500

    added App.js with temporary output

--- industry-reports/src/App.js
+++ industry-reports/src/App.js
@@ -1,69 +1,120 @@
 import React, { Component } from 'react';
 import './App.css';
 import Equality from './components/Equality';
-import DisAbility from './components/Disability'
-import Freedom from './components/Freedom'
-import image from './Assets/newImage.png'
-import Privacy from './components/Privacy';
-import Political from './components/Political'
+import Privacy from './components/Privacy'
+import Disability from './components/Disability';
+
+
 class App extends Component {
-
-  state= {
-    title: 'Disability'
-  }
-
-  disability=(e) =>{
-    console.log("clicked");
-    this.setState({title: e})
-    
-  }
-
-  privacy(){
-    console.log("privacy clicked")
-  }
-
-  political(){
-   
- return ( <Political/>)
-
-    
-   
-  }
-  render() { 
-      const equality = () => {
-      console.log('connected')
-    }
-  
-    const freedom = () =>{
-      console.log("Free Speech");
-      // return <Freedom/>
-      return( <Freedom
-      image=
-      {<img id='image' src={image} alt='science'/>
+  constructor(props){
+    super(props)
+      this.state = {
+        equality: false,
+        privacy: false,
+        political: false,
+        freedom: false,
+        disability: false
       }
-       heading= {
-      <h1 id ='something'> Nondiscrimination and Equality </h1>}
-      para={<p> 
-        As mentioned above, bias and discrimination have become central topics for those concerned with the governance and social impact of AI systems.20 A number of high profile studies have demonstrated that, as in the case of detecting skin color, certain AI systems are inherently discriminatory. Alarming reports have detailed how discrim-inatory algorithms are already deployed in the justice system, wherein judges use these tools for sentencing that purport to predict the likelihood a criminal defendant will reoffend 
-      </p>}
-   />
-      );
-    }
 
- 
+    
+  }
+
+   
+  render() { 
+  //   const checker = () => {
+  //   switch(this.state.equality) {
+  //     case this.state.equality:
+  //       return <Equality />
+        
+      
+  //     case this.state.privacy:
+  //      return <Privacy />
+  //       break;
+  //     default:
+  //       return null;
+  //   }
+  // }
+    
+      const checker = () => {
+        if(this.state === false){
+          return null;
+        } else if(this.state.equality === true){
+          return <Equality />;
+        } else if(this.state.privacy === true){
+          return <Privacy />;
+        }else if(this.state.disability === true){
+          return <Disability />;
+        
+      }}
+      
+    const equality = () => {
+      this.setState({
+        equality: true,
+        privacy: false,
+        political: false,
+        freedom: false,
+        disability: false
+      })
+    }
+    const privacy = () => {
+      this.setState({
+        equality: false,
+        privacy: true,
+        political: false,
+        freedom: false,
+        disability: false
+      })
+    }
+    const political = () => {
+        this.setState({
+          equality: false,
+          privacy: false,
+          political: true,
+          freedom: false,
+          disability: false
+        })
+      }
+
+      const freedom = () => {
+        this.setState({
+          equality: false,
+          privacy: false,
+          political: false,
+          freedom: true,
+          disability: false
+        })
+      }
+
+      const disability = () => {
+        this.setState({
+          equality: false,
+          privacy: false,
+          political: false,
+          freedom: false,
+          disability: true
+        })
+      }
+    
+    
+  
     return (  
+      
       <React.Fragment>
-      <h1> A HUMAN RIGHTS FRAME FOR AI RISKS AND HARM </h1>
+        
+      <h1 id="main"> A HUMAN RIGHTS FRAME FOR AI RISKS AND HARM </h1>
+      {checker()}
       <div className="topic-box">
+        
         <div id='equality' > <h1 onClick={() => equality()} > equality</h1> </div>
-        <div id='political' > <h1 onClick={() =>this.political}> political</h1> </div>
-        <div id='privacy' > <Privacy onClick={()=> this.privacy} /></div>
-        <div id='freedom' ><h1 onClick={() => freedom()} > freedom</h1>   </div>
-        <div id='disability' > <DisAbility clickText={this.disability.bind(this,'Testing')} title={this.state.title} /></div>
+        <div id='political' ><h1 onClick={() => equality()} > political</h1> </div>
+        <div id='privacy' > privacy <h1 onClick={() => privacy()} > privacy</h1> </div>
+        <div id='freedom' > freedom </div>
+        <div id='disability' > <h1 onClick={() => disability()} >disability</h1> </div>
       </div>
       <h1> About </h1>
       </React.Fragment>
     );
+    
     }
   }
   
