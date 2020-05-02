cef70e255c156df51207f3d8198eae43865edf68
dev-augustin <dev.augustin00@gmail.com>
2020-05-02 14:52:09 -0500

    added all updated files with styling and heading

--- industry-reports/src/components/Political.js
+++ industry-reports/src/components/Political.js
@@ -24,16 +24,10 @@ class Political extends React.Component {
                   <h2 id="political-heading"> Political Participation </h2>
                     <h3 id="political-subheading"> Platforms are more likely to remove bots because they violate their terms of service rather than to protect users’ right to political participation. 
                     </h3>
-                      <p id="paragraph-1">  Bots have many useful purposes, including helping search engines find content. Because AI is being designed to mimic human behavior in online conversations, detecting those online bots that are weaponized to spread disinformation in political discourse could become more difficult. Further studies have demonstrated that bots have and continue to be used to manipulate the media in countries across the world to interfere with the outcomes of democratic elections.(35)
+                      <p id="paragraph-1">  Bots have many useful purposes, including helping search engines find content. Because AI is being designed to mimic human behavior in online conversations, detecting those online bots that are weaponized to spread disinformation in political discourse could become more difficult. Further studies have demonstrated that bots have and continue to be used to manipulate the media in countries across the world to interfere with the outcomes of democratic elections.
                       </p>
                       <p id="paragraph-2">
-                      The rights around political participation are referenced, for example, in the right to self-determination and the right to equal participation in political and public affairs in
-                      the ICCPR. Viewed through this human rights lens, the co-opted use of an automated
-                      system by a bad-faith actor creates a human rights liability that demands redress. Yet
-                      finding the right remedy is one of the most contentious areas in platform technology
-                      today. Platforms are more likely to remove bots because they violate their terms of
-                      service rather than to protect users’ right to political participation. Further exploration
-                      would be needed to see how human rights principles could inform contractual disputes or litigation in this context.</p>
+                      The rights around political participation are referenced, for example, in the right to self-determination and the right to equal participation in political and public affairs in the ICCPR. Viewed through this human rights lens, the co-opted use of an automated system by a bad-faith actor creates a human rights liability that demands redress. Yet finding the right remedy is one of the most contentious areas in platform technology today. Platforms are more likely to remove bots because they violate their terms of service rather than to protect users’ right to political participation. Further exploration would be needed to see how human rights principles could inform contractual disputes or litigation in this context.</p>
                   </div>
             </div>
         );
