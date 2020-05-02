be5ad507b573ff57790f68d684c4b653d5128e62
KevinJ.Regis <dev.regis1@gmail.com>
2020-05-02 14:13:49 -0500

    'final' commits for industry reports

--- industry-reports/src/components/Equality.jsx
+++ industry-reports/src/components/Equality.jsx
@@ -8,14 +8,14 @@ const Equality = () => {
       <img id='equality-image' src={image} alt='science'/>
       </div>
       <div>
-      <h1 id ='something'> Nondiscrimination and Equality </h1>
-      <h3>
+      <h1 id="equality-heading"> Nondiscrimination and Equality </h1>
+      <h3 id="equality-subheading">
       Even when there is no intention for discrimination, ML [machine learning] systems for which success is strictly measured in terms of efficiency and profit may end up achieving these at the expense of a company’s responsibility to respect human rights
       </h3>
-      <p> 
+      <p id="paragraph-1"> 
         Bias and discrimination have become central topics for those concerned with the governance and social impact of AI systems.20 A number of high profile studies have demonstrated that, as in the case of detecting skin color, certain AI systems are inherently discriminatory. Alarming reports have detailed how discrim-inatory algorithms are already deployed in the justice system, wherein judges use these tools for sentencing that purport to predict the likelihood a criminal defendant will reoffend.
       </p>
-      <p>
+      <p id="paragraph-2">
       The importance of equality and nondiscrimination has filtered down through the ratifi-cation of treaties to provide the basis for post-war constitutions, state law, and judicial interpretation.27 For example, the South African constitution, adopted in 1996, directly accounted for the discriminatory policies of the past. The constitution establishes equality, human dignity, and human rights as its legal foundations and core values.There have been attempts to frame discrimination in machine learning algorithms as a human rights issue, as in a recent World Economic Forum (WEF) report that raised both concerns and possible solutions for biased decision-making.28 The report calls for human rights to move to the center of AI discussions.
       </p>
       </div>
--- industry-reports/src/components/Privacy.js
+++ industry-reports/src/components/Privacy.js
@@ -10,18 +10,26 @@ export default function Privacy() {
                 <img id="privacy-image" src="https://images.unsplash.com/photo-1557597774-9d273605dfa9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1044&q=80" alt="privacy"></img>
             </div>
             <div>
-                <h1>Privacy</h1>
-                <p>Privacy has long been a major concern for a broad field that includes government,
+                <h1 id="privacy-heading">Privacy</h1>
+                <h3 id="privacy-subheading"> If AI developers treat privacy not as an ehical preference
+                but as a fundamental human right, it would strengthen the privacy
+                considerations that already exist in industry norms and technicaal standards. 
+                </h3>
+                <p id="paragraph-1">
+                Privacy has long been a major concern for a broad field that includes government,
                 business, academia, and civil society organizations. For example, there has been a
                 surge in interest from developers and engineers to follow privacy-by-design36 principles, which demonstrate how norms can be incorporated at the systems-design level.
                 Conducting a privacy impact assessment for technological deployments is an established tool for privacy compliance. Yet we already see tensions around the human
-                right to privacy and AI development.</p>
-                <p>For instance, Stanford University researchers trained a deep neural network to “predict” the sexual orientation of their subjects, without obtaining consent, using a set of
+                right to privacy and AI development.
+                </p>
+
+                <p id="paragraph-2">For instance, Stanford University researchers trained a deep neural network to “predict” the sexual orientation of their subjects, without obtaining consent, using a set of
                 images collected from online dating websites.37 Beyond various methodological shortcomings, the research demonstrated how a disregard for privacy rights increases the
                 risks of algorithmic surveillance, where data that is collected and analyzed threatens
                 to reveal personal information about users. This can put individuals and groups at risk,
                 particularly those living under regimes that would use such information to repress and
-                discriminate.</p>
+                discriminate.
+                </p>
             </div>
         </div>
     )
